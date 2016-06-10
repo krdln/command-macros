03186cbf7cbcb44fbf41246659a7f1f6ed82f9e7
Michał Krasnoborski <mkrdln@gmail.com>
2016-06-10 17:37:58 +0200

    docs

---
+++ command_macros/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["plugin_registrar",""]],"macro":[["cmd!","Simple macro for creating `Command`."],["command!","Full-featured macro for creating `Command`"]]});
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["command_macros"] = {"doc":"Macros for creating\n[`std::process::Command`](https://static.rust-lang.org/doc/master/std/process/struct.Command.html)\nwith shell-like syntax.","items":[[5,"plugin_registrar","command_macros","",null,{"inputs":[{"name":"registry"}],"output":null}],[14,"command!","","Full-featured macro for creating `Command`",null,null],[14,"cmd!","","Simple macro for creating `Command`.",null,null]],"paths":[]};
+initSearch(searchIndex);
